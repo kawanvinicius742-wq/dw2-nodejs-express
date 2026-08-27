d4383d091fe2bef65342761ad110787b1fc1f0e2
Kawan.VPeniche <kawanvinicius742@gmail.com>
2026-08-26 22:41:31 -0300

    att

---
+++ js-aula-02.3-classes/js-Exercicio-Classe/script.js
@@ -0,0 +1,57 @@
+//MÉTODO
+class Heroi{
+    //ATRIBUTO
+    constructor(nome, vida, velocidade, forca){
+        this.nome = nome;
+        this.vida = vida;
+        this.velocidade = velocidade;
+        this.forca = forca
+
+    }
+
+    //MÉTODOS
+  correr() {
+     return "Herói está correndo!";
+  }
+
+  andar() {
+     return "Herói está andando!";
+  }
+
+  atacar() {
+     return "Herói está atacando!";
+  }
+
+  defender() {
+     return "Herói está se defendendo!";
+  }
+}
+
+  //CRIANDO UMA INSTÂNCIA (OBJETO) DA CLASSE CARRO
+
+  //INSTÂNCIA HOMEM ARANHA
+  const homemAranha = new Heroi();
+  homemAranha.nome = "";
+  homemAranha.vida = "";
+  homemAranha.velocidade = "";
+  homemAranha.forca = "";
+
+
+  //INSTÂNCIA SUPERMAN
+  const superman = new Heroi();
+  superman.nome = "";
+  superman.vida = "";
+  superman.velocidade = "";
+  superman.forca = "";
+
+
+  //INSTÂNCIA BATMAN
+  const batman = new Heroi();
+  batman.nome = "";
+  batman.vida = "";
+  batman.velocidade = "";
+  batman.forca = "";
+
+
+  
+
